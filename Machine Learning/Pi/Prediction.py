d02d354f5916dfb3e0dbf4cb07c3901e0a5e7b82
TBY\User <tby1994@gmail.com>
2018-11-07 20:59:48 +0800

    Updated model

--- Machine Learning/Pi/Prediction.py
+++ Machine Learning/Pi/Prediction.py
@@ -18,27 +18,27 @@ from .segmentation import data_processing
 
 def predict():
     filename_mlp = "/home/pi/3002/ml/Neural_Network_Model.sav"
-    filename_svm = "/home/pi/3002/ml/SVM.sav"
     filename_rd = "/home/pi/3002/ml/RD.sav"
     data = pd.read_csv('/home/pi/3002/ml/extracted_data.csv')
-#     data = pd.read_csv(r'C:\Users\User\Desktop\Study\Year 3\Sem 1\CG3002\DanceProject\Machine Learning\extracted data\test_dataset.csv')
-#     data = pd.read_csv(r'C:\Users\User\Desktop\chicken_extracted_dataset.csv')#
+    
     mlp_model = pickle.load(open(filename_mlp, 'rb'))
-    svm_model = pickle.load(open(filename_svm, 'rb'))
     rd_model = pickle.load(open(filename_rd, 'rb'))
+    
+    [voltage, current, power, energy] = data_processing(2)
     
     while(True):
         prediction_mlp = mlp_model.predict(data)
-        prediction_svm = svm_model.predict(data)
         prediction_rd = rd_model.predict(data)
 		print(prediction_mlp, prediction_rd, prediction_svm)
-        if ((prediction_mlp == prediction_svm == prediction_rd) & (prediction_mlp != 0)):
+        if ((prediction_mlp == prediction_rd) & (prediction_mlp != 0)):
             break
-        data_processing(0.2)
+        [voltage, current, power, energy] = data_processing(0.2)
 		data = pd.read_csv('/home/pi/3002/ml/extracted_data.csv')
         
     mapping = {1 : 'chicken', 2 : 'number7', 3 : 'sidestep', 4 : 'turnclap', 5 : 'wipers',
                6 : 'numbersix', 7 : 'salute', 8 : 'mermaid', 9 : 'swing', 10 : 'cowboy', 11 : 'finisher'}
-    return mapping[prediction_mlp[0]]
+    
+    result = mapping[prediction_mlp[0]]
+    return [voltage, current, power, energy, result]
 
 
--- Machine Learning/Pi/Segmentation.py
+++ Machine Learning/Pi/Segmentation.py
@@ -14,32 +14,30 @@ from sklearn import preprocessing
 
 import datetime
 
-def data_processing(time):
+def data_processing(a):
     start = datetime.datetime.now()
-    time.sleep(time)
+    time.sleep(a)
     # read data
+    
     data = pd.read_csv('/home/pi/3002/readings.csv')
-#     data = pd.read_csv(r'C:\Users\User\Desktop\chicken_dataset.csv')
-    data = data.tail(55)
+    data = data.tail(110)
     cleaned_data = data.drop(['voltage','current','power','energy'],axis =1).copy() #delete column
-    cleaned_data = cleaned_data.astype(float)
-    cleaned_data = pd.DataFrame(preprocessing.scale(cleaned_data),
-                                                   columns=['AcX 1', 'AcY 1', 'AcZ 1', 'GyX 1', 'GyY 1', 'GyZ 1',                                                           
-                                                            'AcX 2', 'AcY 2', 'AcZ 2', 'GyX 2', 'GyY 2', 'GyZ 2',
-                                                            'AcX 3', 'AcY 3', 'AcZ 3', 'GyX 3', 'GyY 3', 'GyZ 3'])
     
     mean_data = pd.DataFrame() 
     max_data = pd.DataFrame()
-    iqr_data = pd.DataFrame() 
+    iqr_data = pd.DataFrame()
+    mad_data = pd.DataFrame()
 
-    a = cleaned_data[0:55].copy()
-    a.loc['mean'] = a.mean()
-    a.loc['max'] = a.max()
-    a.loc['iqr'] = a.quantile(0.75) - a.quantile(0.25)
 
-    mean_data = mean_data.append(a.loc['mean'], ignore_index=True)
-    max_data = max_data.append(a.loc['max'], ignore_index=True)
-    iqr_data = iqr_data.append(a.loc['iqr'], ignore_index=True)
+    cleaned_data.loc['mean'] = cleaned_data.mean()
+    cleaned_data.loc['max'] = cleaned_data.max()
+    cleaned_data.loc['iqr'] = cleaned_data.quantile(0.75) - cleaned_data.quantile(0.25)
+    cleaned_data.loc['mad'] = cleaned_data.mad()
+
+    mean_data = mean_data.append(cleaned_data.loc['mean'], ignore_index=True)
+    max_data = max_data.append(cleaned_data.loc['max'], ignore_index=True)
+    iqr_data = iqr_data.append(cleaned_data.loc['iqr'], ignore_index=True)
+    mad_data = mad_data.append(cleaned_data.loc['mad'], ignore_index=True)
 
     # rearrangement of columns
     mean_data = mean_data.rename(index=str, columns={'AcX 1': 'mean_AcX 1', 'AcY 1': 'mean_AcY 1', 'AcZ 1': 'mean_AcZ 1', 
@@ -63,15 +61,16 @@ def data_processing(time):
                                                      'AcX 3': 'iqr_AcX 3', 'AcY 3': 'iqr_AcY 3', 'AcZ 3': 'iqr_AcZ 3', 
                                                      'GyX 3': 'iqr_GyX 3', 'GyY 3': 'iqr_GyY 3', 'GyZ 3': 'iqr_GyZ 3',}) 
     
+    mad_data = mad_data.rename(index=str, columns=  {'AcX 1': 'mad_AcX 1', 'AcY 1': 'mad_AcY 1', 'AcZ 1': 'mad_AcZ 1', 
+                                                     'GyX 1': 'mad_GyX 1', 'GyY 1': 'mad_GyY 1', 'GyZ 1': 'mad_GyZ 1',
+                                                     'AcX 2': 'mad_AcX 2', 'AcY 2': 'mad_AcY 2', 'AcZ 2': 'mad_AcZ 2', 
+                                                     'GyX 2': 'mad_GyX 2', 'GyY 2': 'mad_GyY 2', 'GyZ 2': 'mad_GyZ 2',
+                                                     'AcX 3': 'mad_AcX 3', 'AcY 3': 'mad_AcY 3', 'AcZ 3': 'mad_AcZ 3', 
+                                                     'GyX 3': 'mad_GyX 3', 'GyY 3': 'mad_GyY 3', 'GyZ 3': 'mad_GyZ 3',})    
     
     # combine features extracted into 1 
-    extracted_data = mean_data.join(max_data).join(iqr_data)
+    extracted_data = mean_data.join(max_data).join(iqr_data).join(mad_data)
 
-    # store row 110
-#     data = pd.read_csv(r'C:\Users\User\Desktop\chicken_dataset.csv')
-#     data = data[220::].copy()
-#     data.to_csv(r'C:\Users\User\Desktop\chicken_dataset.csv',index=False)
-#     extracted_data.to_csv(r'C:\Users\User\Desktop\chicken_extracted_dataset.csv', index=False)
     extracted_data.to_csv('/home/pi/3002/ml/extracted_data.csv', index=False)
     voltage = data['voltage'].iloc[-1]
     current = data['current'].iloc[-1]
